12abb31d8b044db2ef033da7c15f70dbdb2fe25a
Richard <richardkummer8@gmail.com>
2021-03-10 09:44:43 -0500

    Did styling for download button

--- scripts/menu.js
+++ scripts/menu.js
@@ -8,7 +8,6 @@ if (navigator.userAgent.match(/Android/i)
     || navigator.userAgent.match(/iPod/i)
     || navigator.userAgent.match(/BlackBerry/i)
     || navigator.userAgent.match(/Windows Phone/i)
-    || true
 ) {
     // use pdf download button
     pdfEmbed.style.display = "none";
